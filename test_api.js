f5d67f2d5074af8b5a52f95a5aff280380d7f214
Vitnesh Immanuvel <vitneshimmanuvel@gmail.com>
2026-04-25 10:55:18 +0530

    comit

--- test_api.js
+++ test_api.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const token = jwt.sign({ id: '33647315-c0d8-456f-89f9-f148f752b371', userType: 'vendor' }, process.env.JWT_SECRET || 'your_jwt_secret');
-//edgdfsfsfdbdbbffffs
+//edgdfsfsfdbdbbf
 async function testApi() {
     try {
         const url = 'http://localhost:3000/api/vendor/trips?status=pending,driver_accepted,vendor_approved,confirmed,started,in_progress,arrived,dropped,pickup,drop,payment_pending,payment_verification_pending,commission_pending,commission_verification_pending';
